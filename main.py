c8be5c733144e6c7d6daa1909e599b747e1859f9
CupKido <taler@g.jct.ac.il>
2023-04-25 19:10:30 +0300

    added hebrew decoding

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import json
 from izkor_wrapper import izkor_wrapper
 app = Flask(__name__)
 
@@ -8,7 +9,15 @@ def get_halalim_by_name():
     last_name = request.args.get('last_name')
     halalim = izkor_wrapper.get_halalim_by_name(first_name, last_name)
     halalim_jsons = list(map(lambda x: x.__dict__, halalim))
-    return jsonify(halalim_jsons), 200, {'Content-Type': 'application/json; charset=utf-8'}
+    json_data = json.dumps(halalim_jsons)
+    decoded_data = json_data.encode('utf-8').decode('unicode_escape')
+    response = app.response_class(
+        response=decoded_data,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+    #return halalim_jsons, 200, {'Content-Type': 'application/json; charset=utf-8'}
 
 @app.route('/GetHalal', methods=['GET'])
 def get_halal_by_id():
@@ -16,7 +25,14 @@ def get_halal_by_id():
     if id == '':
         return None, 404
     halal = izkor_wrapper.get_halal_by_id(id)
-    return jsonify(halal.__dict__), 200, {'Content-Type': 'application/json; charset=utf-8'}
+    json_data = json.dumps(halal.__dict__)
+    decoded_data = json_data.encode('utf-8').decode('unicode_escape')
+    response = app.response_class(
+        response=decoded_data,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(port=3500, debug=True)
